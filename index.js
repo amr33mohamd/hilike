f4f4f7b0a1faf463e74f454ea92890370d109fff
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-07-31 23:12:58 +0200

    amr's touch

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+/*
+// --------------------- Read Comments we are helping you ------------------
+
+You need to know about :
+redux
+axios
+react navigation
+and understannd the following concepts:
+state
+props
+components
+//we worked hard to make easy examples just to help you learn so
+don't always ask us make google your best friend :D
+*/
