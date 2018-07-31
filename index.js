8961394cf56e3dc14aa36231984b10070861fad4
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-07-31 23:15:24 +0200

    amr's touch

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ and understannd the following concepts:
 state
 props
 components
+
+
 //we worked hard to make easy examples just to help you learn so
 don't always ask us make google your best friend :D
+we will get a report from you every day and what you did so if you find your self
+not doning fine you need to contact wheather (amr or karim) because if you just
+burn our time you will never work with us again
+We care alot about clients reviews that's why we are aggresive in that
 */
