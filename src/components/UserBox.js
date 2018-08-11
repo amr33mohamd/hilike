e7da9370636e64fb869b8b45de6aedadb8760392
amr33mohamd <amr2010mohamd2010@gmail.com>
2018-08-11 19:59:58 +0200

    searc

--- src/components/UserBox.js
+++ src/components/UserBox.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { Button, Text } from 'native-base';
 import {View,I18nManager,ImageBackground} from "react-native";
 import { strings,currentLocale } from '../i18n';
-
-export default class Text2 extends Component {
+import {Icon} from 'native-base'
+export default class UserBox extends Component {
     render() {
         return (
-          <ImageBackground source={{uri:'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2014/10/dani-diamond-posing-how-to-model-photogaphy-best-top-fstoppersb.jpg'}} style={{flex: 1, height: 130,borderRadius: 10, marginHorizontal: 3, backgroundColor: 'white' }}>
+          <ImageBackground source={{uri:'https://image.ibb.co/mRPfMK/10m.png'}} style={{flex: 1, height: 130,borderRadius: 10, marginHorizontal: 3, backgroundColor: 'white' }}>
             <View style={{flexDirection:'row'}}>
-              <Text style={{color:'white',textAlign:'left',alignSelf:'flex-start',flex:.5}}>Inside</Text>
-              <Text style={{color:'white',textAlign:'right',alignSelf:'flex-end',flex:.5}}>Inside</Text>
+              <Text style={{color:'white',textAlign:'left',alignSelf:'flex-start',flex:.5,fontFamily:'Droid Sans',fontSize:20}}>1 KM</Text>
+              <Text style={{color:'white',textAlign:'right',alignSelf:'flex-end',flex:.5}}><Icon style={{color:'green'}} size={30} type="Entypo" name="dot-single"/></Text>
             </View>
-            <Text style={{color:'white',textAlign:'right',alignSelf:'flex-end',flex:.5,bottom:0,position:'absolute'}}>Amr Mohmed , 25 years old</Text>
+            <Text style={{color:'white',textAlign:'right',alignSelf:'flex-end',fontFamily:'Droid Sans',fontWeight:'bold',fontSize:12,padding:8,flex:.5,bottom:0,position:'absolute'}}>Amr Mohmed , 25 years old</Text>
           </ImageBackground>
 
         );
--- src/screens/app/Search/index.js
+++ src/screens/app/Search/index.js
@@ -1,15 +1,70 @@
 import React, { Component } from 'react';
-import {Text} from 'native-base';
+import {Text,View,Switch,List,Left,Button,Body,Right,ListItem,Icon} from 'native-base';
 import AppTemplate from "../appTemplate";
 import componentExample from "../../../components/componentExample";
 import Colors from "../../../constants/colors";
-
+import Text2 from "../../../components/Text2";
+import {Slider} from 'react-native'
 export default class SearchScreen extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      image:true,
+      last_seen:3
+    }
+  }
     render() {
           const nav = this.props.navigation
             return (
-            <AppTemplate navigation={nav}>
-                <Text style={{color: Colors.mainColor}}>search</Text>
+            <AppTemplate navigation={nav} name="navigator.search">
+            <View style={{justifyContent:'center',flexDirection:'row',alignItems:'center',backgroundColor:'#fafafb',height:75,borderWidth:.5,borderColor:'#c3c3c3'}}>
+              <Text2 style={{color: 'black',flex:.9,fontSize:17,fontWeight:'bold'}} text="navigator.search"/>
+
+            </View>
+              <List>
+                <ListItem>
+                  <Text2  text="navigator.search"/>
+                  <Switch
+                  style={{flex:1}} onValueChange={(value)=>{
+                    this.setState({image:value})
+                  }} value={this.state.image} />
+                </ListItem>
+                <ListItem >
+                  <Text2 style={{paddingRight:10}} text="navigator.search"/>
+                  <Slider
+                  style={{
+                    flex:1,
+                    paddingHorizontal:10
+                    }} onValueChange={(value)=>{
+                    this.setState({last_seen:value})
+                  }} value={this.state.last_seen}
+
+                  maximumValue={10}
+                  minimumValue={1} />
+                  <Text style={{paddingHorizontal:10}}>{Math.round(this.state.last_seen)} يوم </Text>
+                </ListItem>
+              </List>
+              <View style={{justifyContent:'center',alignItems:'center',backgroundColor:'#fafafb',flexDirection:'row',height:75,borderWidth:.5,borderColor:'#c3c3c3'}}>
+                <Text2 style={{color: 'black',flex:.8,fontSize:17,fontWeight:'bold'}} text="navigator.search"/>
+                <Icon name="lock" style={{flex:.1,fontSize:32,fontWeight:'bold'}} />
+              </View>
+              <List>
+              <ListItem>
+                <Text2 style={{paddingRight:10}} text="navigator.search"/>
+                <Slider
+                style={{
+                  flex:1,
+                  paddingHorizontal:10
+                  }} onValueChange={(value)=>{
+                  this.setState({last_seen:value})
+                }} value={this.state.last_seen}
+
+                maximumValue={10}
+                minimumValue={1} />
+                <Text style={{paddingHorizontal:10}}>{Math.round(this.state.last_seen)} يوم </Text>
+              </ListItem>
+
+              </List>
             </AppTemplate>
         );
     }
